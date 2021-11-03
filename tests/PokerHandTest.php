c15b6be8f49263625aa65241dd82bfb0b1c99b35
Clovis Guillemot <clovis.guillemot@gmail.com>
2021-11-03 09:03:12 +0100

    Add unit Test for Pair/2Pairs

--- tests/PokerHandTest.php
+++ tests/PokerHandTest.php
@@ -4,7 +4,6 @@ namespace App\Tests;
 
 use App\Entity\PokerHand;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\Form\Exception\InvalidArgumentException;
 
 class PokerHandTest extends TestCase
 {
@@ -34,5 +33,14 @@ class PokerHandTest extends TestCase
     {
         $hand = new PokerHand("KS 2H 5C JD TD");
         $this->assertSame(3, $hand->compareWith("KS 2H 5C JD TD"));
+    }
+
+    /**
+     * Test One Pair / Two Pair.
+     */
+    public function testOnePairTwoPair(): void
+    {
+        $hand = new PokerHand("AS 2H 5H TH TD"); // One Pair of Ten
+        $this->assertSame(2, $hand->compareWith("JH 5C 5S 7H 7D")); // Two Pairs
     }
 }
